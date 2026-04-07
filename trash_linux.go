9c95e3ac0c3004fa757509df764291eaa17464ac
Shawn <rafshawn@yandex.com>
2026-04-07 01:33:03 -0400

    handle file collision in trash, implement actually deleting file, and adding .trashinfo

--- trash_linux.go
+++ trash_linux.go
@@ -4,6 +4,8 @@ import (
 	"fmt"
 	"os"
 	"path/filepath"
+	"strings"
+	"time"
 )
 
 func TrashItem(filePath string) error {
@@ -16,7 +18,7 @@ func TrashItem(filePath string) error {
 		return fmt.Errorf("go2trash: file does not exist: %w", err)
 	}
 
-	home, err := filepath.Abs(filePath)
+	home, err := os.UserHomeDir()
 	if err != nil {
 		return fmt.Errorf("go2trash: cannot find home directory: %w", err)
 	}
@@ -33,5 +35,51 @@ func TrashItem(filePath string) error {
 		return fmt.Errorf("go2trash: cannot create trash info dir: %w", err)
 	}
 
+	fileName := filepath.Base(absPath)
+	trashName := fileName
+
+	infoCheck := filepath.Join(infoDir, trashName+".trashinfo")
+	if _, err := os.Stat(infoCheck); err == nil {
+		trashName = appendTime(fileName)
+	}
+
+	trashFile := filepath.Join(filesDir, trashName)
+	trashInfo := filepath.Join(infoDir, trashName+".trashinfo")
+
+	infoContent := fmt.Sprintf(
+		"[Trash Info]\nPath=%s\nDeletionDate=%s\n",
+		absPath,
+		trashTime(),
+	)
+
+	f, err := os.OpenFile(trashInfo, os.O_WRONLY|os.O_CREATE|os.O_EXCL, 0644)
+	if err != nil {
+		return fmt.Errorf("go2trash: cannot create trashinfo: %w", err)
+	}
+	f.WriteString(infoContent)
+	f.Close()
+
+	// move file to the trash files directory
+	if err := os.Rename(absPath, trashFile); err != nil {
+		os.Remove(trashInfo) // rollback on failure
+		return fmt.Errorf("go2trash: cannot move file to trash: %w", err)
+	}
+
 	return nil
+}
+
+// Add timestamp to avoid collision in trash dir
+func appendTime(name string) string {
+	ext := filepath.Ext(name)
+	fileName := strings.TrimSuffix(name, ext)
+
+	t := time.Now()
+	timeStr := t.Format("03.04.05 PM")
+
+	return fileName + " " + timeStr + ext
+}
+
+// proper timestamp format for .trashinfo
+func trashTime() string {
+	return time.Now().Format("20060102T03:04:05")
 }
